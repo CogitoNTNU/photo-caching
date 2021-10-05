62822507626f43d8a787b572b12788e9cb57ab96
Julian <42567826+Jotto2@users.noreply.github.com>
2021-10-05 21:43:43 +0200

    Revert "#5 La til kart"

--- app/App.js
+++ app/App.js
@@ -6,12 +6,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
-import MapScreen from './screens/MapScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <MapScreen />
+      <HomeScreen/>
     </Provider>
   );
 }
