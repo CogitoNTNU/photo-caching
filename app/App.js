3b9e1f06a126d524188177da734429c9bc8f7627
Julian Ammouche Ottosen <julianao@stud.ntnu.no>
2021-11-10 17:49:31 +0100

    Lagt til intro skjerm

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 //Create store.js
@@ -8,13 +8,39 @@ import HomeScreen from './screens/HomeScreen';
 import IntroScreen from './screens/IntroScreen';
 import { store } from './store';
 import MapScreen from './screens/MapScreen';
+//import AsyncStorage from '@react-native-community/async-storage';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <IntroScreen />
-    </Provider>
-  );
+    <IntroScreen />
+  )
+  
+  /*const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value == null){
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    })
+  }, []);
+
+  if ( isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch === true){ // FIRST LAUNCH
+    return (
+      <Provider store={store}>
+        <MapScreen />
+      </Provider>
+    )
+  } else { // NOT FIRST LAUNCH
+    return <Provider store={store}>
+        <IntroScreen />
+      </Provider>
+  }*/
 }
 
 const styles = StyleSheet.create({
